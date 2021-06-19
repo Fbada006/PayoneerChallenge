6ec0ccf17f6c22f16739adaa1eb87db4d2bb220f
Fbada006 <ferdinandkamata@yahoo.com>
2021-06-19 13:54:49 +0300

    add nav graph and home and payment frags

--- app/src/main/java/com/payoneer/payoneerchallenge/MainActivity.java
+++ app/src/main/java/com/payoneer/payoneerchallenge/MainActivity.java
@@ -1,13 +1,17 @@
 package com.payoneer.payoneerchallenge;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.payoneer.payoneerchallenge.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
