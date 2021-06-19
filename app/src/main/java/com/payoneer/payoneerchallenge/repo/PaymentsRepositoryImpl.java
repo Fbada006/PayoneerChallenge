a29b768424183200ac0c8f711dfc29505cd2d456
Fbada006 <ferdinandkamata@yahoo.com>
2021-06-19 19:11:15 +0300

    hook up rxjava with network layer

--- app/src/main/java/com/payoneer/payoneerchallenge/repo/PaymentsRepositoryImpl.java
+++ app/src/main/java/com/payoneer/payoneerchallenge/repo/PaymentsRepositoryImpl.java
@@ -4,9 +4,12 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.LiveDataReactiveStreams;
 import androidx.lifecycle.MediatorLiveData;
 import com.payoneer.payoneerchallenge.network.api.PaymentsService;
+import com.payoneer.payoneerchallenge.network.models.ApplicableItem;
 import com.payoneer.payoneerchallenge.network.models.Networks;
 import com.payoneer.payoneerchallenge.utils.Resource;
 import io.reactivex.schedulers.Schedulers;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 
 public class PaymentsRepositoryImpl implements PaymentsRepository {
@@ -24,13 +27,16 @@ public class PaymentsRepositoryImpl implements PaymentsRepository {
     public LiveData<Resource<Networks>> getPayments() {
         networks.setValue(Resource.loading());
 
-        final LiveData<Resource<?>> source =
+        final LiveData<Resource<Networks>> source =
                 LiveDataReactiveStreams.fromPublisher(
                         paymentsService.getPaymentNetworks()
-                                .doOnError(throwable -> Resource.error(throwable.getMessage(), null))
+                                .onErrorReturn(throwable -> {
+                                    List<ApplicableItem> emptyList = new ArrayList<>();
+                                    return new Networks(emptyList);
+                                })
                                 .map(response -> {
                                     if (response.getApplicable().isEmpty()) {
-                                        return Resource.error("Empty list", null);
+                                        return Resource.error("Empty list", response);
                                     }
                                     return Resource.success(response);
                                 })
