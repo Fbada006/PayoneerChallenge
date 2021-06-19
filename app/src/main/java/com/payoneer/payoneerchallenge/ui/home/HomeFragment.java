24d49ddedce7381c3b8cf0797597e2bc01838bd1
Fbada006 <ferdinandkamata@yahoo.com>
2021-06-19 14:16:42 +0300

    hook up basic nav from home to payments list

--- app/src/main/java/com/payoneer/payoneerchallenge/ui/home/HomeFragment.java
+++ app/src/main/java/com/payoneer/payoneerchallenge/ui/home/HomeFragment.java
@@ -4,19 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import com.payoneer.payoneerchallenge.R;
+import com.payoneer.payoneerchallenge.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(getLayoutInflater(), container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.buttonCheckout.setOnClickListener(v -> {
+            Navigation.findNavController(view).navigate(
+                    R.id.action_homeFragment_to_paymentListFragment
+            );
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
