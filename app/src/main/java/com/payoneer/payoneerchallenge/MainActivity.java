58482862ed115ad08ce7a8771c45c2dcc499002f
Fbada006 <ferdinandkamata@yahoo.com>
2021-06-19 13:24:25 +0300

    Initial commit

---
+++ app/src/main/java/com/payoneer/payoneerchallenge/MainActivity.java
@@ -0,0 +1,13 @@
+package com.payoneer.payoneerchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
