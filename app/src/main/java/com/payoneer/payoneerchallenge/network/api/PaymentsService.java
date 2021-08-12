038a7157fd0dbea3d033b109c40e60de38441ca2
Fbada006 <ferdinandkamata@yahoo.com>
2021-08-12 16:24:25 +0300

    add data for posting payment json to the server

--- app/src/main/java/com/payoneer/payoneerchallenge/network/api/PaymentsService.java
+++ app/src/main/java/com/payoneer/payoneerchallenge/network/api/PaymentsService.java
@@ -1,11 +1,18 @@
 package com.payoneer.payoneerchallenge.network.api;
 
 import com.payoneer.payoneerchallenge.models.PaymentResponse;
+import com.payoneer.payoneerchallenge.models.PostPaymentResponse;
 import io.reactivex.Flowable;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Url;
 
 public interface PaymentsService {
 
     @GET("optile/checkout-android/develop/shared-test/lists/listresult.json")
     Flowable<PaymentResponse> getPaymentNetworks();
+
+    @POST
+    Flowable<PostPaymentResponse> postPaymentDetails(@Url String postPaymentUrl, @Body String paymentDetailJson);
 }
--- app/src/main/java/com/payoneer/payoneerchallenge/repo/PaymentsRepository.java
+++ app/src/main/java/com/payoneer/payoneerchallenge/repo/PaymentsRepository.java
@@ -2,8 +2,10 @@ package com.payoneer.payoneerchallenge.repo;
 
 import androidx.lifecycle.LiveData;
 import com.payoneer.payoneerchallenge.models.PaymentResponse;
+import com.payoneer.payoneerchallenge.models.PostPaymentResponse;
 import com.payoneer.payoneerchallenge.utils.Resource;
 
 public interface PaymentsRepository {
     LiveData<Resource<PaymentResponse>> getPayments();
+    LiveData<Resource<PostPaymentResponse>> postPaymentDetails(String postUrl, String paymentJson);
 }
